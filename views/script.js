8c138d908db4995efcc964f822695ff726daeb0a
Amber <woll.ambermadison@gmail.com>
2024-12-09 22:40:30 -0700

    login html and script.js
    removed sign up

--- views/script.js
+++ views/script.js
@@ -25,20 +25,6 @@
         }
     });
 
-    function toggleForms() {
-        const loginForm = document.getElementById("loginForm");
-        const signUpForm = document.getElementById("signUpForm");
-    
-        if (loginForm.style.display === "none") {
-            loginForm.style.display = "block";
-            signUpForm.style.display = "none";
-        } else {
-            loginForm.style.display = "none";
-            signUpForm.style.display = "block";
-        }
-    }
-    
-
 
 
 
